2b61dba19b58a8be12b00108f4b243e985b82d0c
hbulou <herve.bulou@ipcms.unistra.fr>
2018-03-21 23:04:45 +0100

    Adding GramSchmidt.c

---
+++ GramSchmidt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+
+double dot(int N,double *vec1,double *vec2);
+
+
+
+
+double **GramSchmidt(int N,double **set0,int nvec0,double *set1){
+  double **out;out=malloc((nvec0+1)*sizeof(double*));
+  int i,j,l;
+  double alpha;
+  for(j=0;j<nvec0+1;j++)  out[j]=malloc(N*sizeof(double));
+  for(j=0;j<nvec0;j++){
+    alpha=dot(N,set0[j],set1);
+    for(l=0;l<N;l++) {
+      set1[l]-=alpha*set0[j][l];
+      out[j][l]=set0[j][l];
+    }
+    alpha=dot(N,set1,set1);  alpha=pow(alpha,.5);  for(l=0;l<N;l++)     out[nvec0][l]=set1[l]/alpha;
+  }
+  return out;
+}
